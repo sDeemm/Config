0ccebe93fa51defbee6a361953e7f4f3b8c21549
Sebastien Demers <deemm@deemm>
2016-09-06 11:09:48 -0400

    updated neotree-settings

--- .emacs.d/lisp/neotree-settings.el
+++ .emacs.d/lisp/neotree-settings.el
@@ -2,4 +2,15 @@
 (require 'neotree)
 (global-set-key "\C-xt" 'neotree-toggle)
 
+;; neo-smart-open
+(setq neo-smart-open t)
+
+;; Use with evil-mode
+(add-hook 'neotree-mode-hook
+	  (lambda ()
+	    (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+	    (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+	    (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+	    (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+
 (provide 'neotree-settings)
